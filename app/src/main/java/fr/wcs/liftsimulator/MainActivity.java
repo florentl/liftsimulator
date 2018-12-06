cab6d4f9ee3ad9e1fa487fa94466cf424158b79a
flo <flo33.lav@gmail.com>
2018-12-06 19:56:35 +0100

    lift simulator

--- app/src/main/java/fr/wcs/liftsimulator/MainActivity.java
+++ app/src/main/java/fr/wcs/liftsimulator/MainActivity.java
@@ -1,44 +1,33 @@
 package fr.wcs.liftsimulator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.GridLayout;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.stream.IntStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int TIME_BETWEEN_FLOORS = 3000;
     private boolean isLiftMoving = false;
     private int currentFloor = 0;
 
     private void goToFloor(int floor) {
         if (!isLiftMoving && floor != currentFloor) {
             moveNextFloor(floor);
-            isLiftMoving = false;
         }
     }
 
     private void moveNextFloor(int floor) {
         if (floor != currentFloor) {
             isLiftMoving = true;
-            waitForIt();
-            currentFloor = (floor > currentFloor) ? currentFloor + 1 : currentFloor - 1;
-            TextView floorCount = findViewById(R.id.floor_count);
-            floorCount.setText(String.valueOf(currentFloor));
-            moveNextFloor(floor);
-        }
-    }
-
-    private void waitForIt() {
-        try {
-            Thread.sleep( 0 );
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            //waitForIt();
+            new MoveLift().execute( floor );
+        } else {
+            isLiftMoving = false;
         }
     }
 
@@ -53,4 +42,28 @@ public class MainActivity extends AppCompatActivity {
                     Integer.valueOf( ((Button)v).getText().toString())));
         }
     }
+
+    private class MoveLift extends AsyncTask<Integer,Void,Integer> {
+
+        @Override
+        protected void onPostExecute(Integer targetFloor) {
+            super.onPostExecute( targetFloor );
+            currentFloor = (targetFloor > currentFloor) ? currentFloor + 1 : currentFloor - 1;
+            TextView floorCount = findViewById(R.id.floor_count);
+            floorCount.setText(String.valueOf(currentFloor));
+            moveNextFloor(targetFloor);
+        }
+
+        @Override
+        protected Integer doInBackground(Integer... integers) {
+            try {
+                Thread.sleep( TIME_BETWEEN_FLOORS );
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return integers[0];
+        }
+    }
+
+
 }
